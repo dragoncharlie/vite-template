ae1f34f15dd342f0f5953067ba9ee6b15d7399aa
Dragon <dragoncharlie@yandex.ru>
2023-07-19 10:58:17 +0200

    add language detector

--- src/_i18n/i18n.ts
+++ src/_i18n/i18n.ts
@@ -2,13 +2,15 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import resources from './resourses.ts'
 import { i18n as config } from './i18n-config.ts'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 void i18n
+	.use(LanguageDetector)
 	.use(initReactI18next) // passes i18n down to react-i18next
 	.init({
 		defaultNS: 'index',
 		resources,
-		lng: config.defaultLocale,
+		fallbackLng: config.defaultLocale,
 		interpolation: {
 			escapeValue: false, // react already safes from xss
 		},
